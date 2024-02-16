3de211a07e84510c16641d33eac7d031b4200e74
adrianyu91 <adrianyu91@gmail.com>
2024-02-16 14:57:04 -0500

    Removed default headings

--- src/main/java/AirlineManagement/FlightsPrototype.java
+++ src/main/java/AirlineManagement/FlightsPrototype.java
@@ -1,16 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package AirlineManagement;
 
 import java.util.ArrayList;
 
-/**
- *
- * @author student
- */
 public class FlightsPrototype {
     private static FlightsPrototype fltsObject;
     private ArrayList<Flight> flights = new ArrayList<>();
--- src/main/java/AirlineManagement/User.java
+++ src/main/java/AirlineManagement/User.java
@@ -1,17 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package AirlineManagement;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 
-/**
- *
- * @author student
- */
 public class User {
     String username, password, email, dob, firstName, middleName, lastName, citizenship;
 
